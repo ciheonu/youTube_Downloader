b515ff36b9d896b7ab7a615bef21757be63532a0
ciheonu <98788272+ciheonu@users.noreply.github.com>
2022-05-27 10:45:17 +0100

    Update settings.py

--- youTube/settings.py
+++ youTube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9=5h)m$(!ytc9%ao7^o2km89xjp*@9_&=)6d$m71*s&))_b2uv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
